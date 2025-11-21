6ca0f85f07e47d40ac64fc094abfa6499f1c1367
Jennie Whittaker <jenniekwhittaker@gmail.com>
2025-11-21 12:40:51 +0000

    Added division game challenge functionality.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -21,8 +21,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
     
-    
-
     runGame("addition");
 })
 /**
@@ -46,6 +44,12 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        let num1 = Math.floor(Math.random() * 10) + 1;
+        let num2 = Math.floor(Math.random() * 10) + 1;
+        const dividend = num1 * num2; // ensures dividend is a multiple of divisor
+        const divisor = num1;
+        displayDivisionQuestion(dividend, divisor);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unnknown game type: ${gameType}. Aborting!`;
@@ -90,6 +94,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -137,4 +143,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
